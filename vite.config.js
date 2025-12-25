05c7cecce0d25554aaf59c2bf05d93cfb8bb5991
Tiger Wu <tigerwu@umich.edu>
2025-12-25 15:38:40 -0500

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    strictPort: true
-  }
-});
+  base: '/v65-site/', 
+})
 
 
 
